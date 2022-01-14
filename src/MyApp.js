20ed174229b26b3da1ec10ff2276319c0bc7af90
cchun01 <63212924+cchun01@users.noreply.github.com>
2022-01-13 17:45:02 -0800

    latest commit

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Table from './Table';
 import Form from './Form';
+import axios from 'axios';
 
 function MyApp() { 
   const [characters, setCharacters] = useState([]);
@@ -14,6 +15,25 @@ function MyApp() {
   function updateList(person){
     setCharacters([...characters, person]);
   }
+  
+  useEffect(() => {
+    fetchAll().then( result => {
+       if (result)
+          setCharacters(result);
+     });
+  }, [] );
+
+  async function fetchAll(){
+    try{
+      const response = await axios.get('http://localhost:5000/users');
+      return response.data.users_list;
+    }
+    catch(error){
+      //We're not handling erors. Just logging into the console.
+      console.log(error);
+      return false;
+    }
+  }
 
   return ( 
     <div className='container'>
@@ -22,6 +42,8 @@ function MyApp() {
     </div> 
   );  
 }   
+
+
 
 
 export default MyApp
